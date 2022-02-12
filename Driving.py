49be179473e2a9267bf76b6ca34f5617badd0d21
MichaelHarden <lego4me10@gmail.com>
2022-02-12 13:56:31 -0600

    Added standard deviation. Also cleaned up the trip calculator to be one function to take in a param n for the number of trips

--- Driving.py
+++ Driving.py
@@ -1,21 +1,25 @@
 from Driving_data import cities 
 from random import random 
+import numpy as np
 
-# drive generages a random path between the cities 'A' to 'P' and returns the
-# distance traveled over that path.
 def drive():
-    dist_traveled = 0   # tracks the distance taken to the current city
-    current_city = cities['A']      # tracks the city
-    while current_city != cities['P']: # loops unitl the city p is reached
+    """
+    drive generages a random path between the cities 'A' to 'P', following the
+    provided probabilites. Then, returns the distance traveled over that path.
+    """
+    dist_traveled = 0   # tracks the distance traveled
+    current_city = cities['A']      # tracks the current city
+    while current_city != cities['P']: # loops unitl the city P is reached
         choice = random()   
         neighboring_cities = current_city['neighbors']  
 
-        # if the random number choosen is less than the smaller probabilistic 
+        # if the chosen random number is less than the smaller probabilistic 
         # neighbor, then that is the next city. Otherwise the larger
-        # probabilistic neighbor is choosen as the next city. 
+        # probabilistic neighbor is the next city. 
         #
         # the distance which is required to travel from the current city to
-        # the next city is added to dist_traveled.
+        # the next city is added to dist_traveled. and current_city is added
+        # to dist_traveled
         if choice <= current_city[neighboring_cities[0]]['prob']:
             dist_traveled += current_city[neighboring_cities[0]]['distance']
             current_city = cities[neighboring_cities[0]]
@@ -24,19 +28,43 @@ def drive():
             current_city = cities[neighboring_cities[1]]
     return dist_traveled
 
+def n_drives(n):
+    """
+    calculates average driving distance and standard deviation over n drives.
+    this information is then returned as a tuple.
+
+    Parameters: 
+        n: number of drives
+
+    Return:
+        tuple() 
+            [0]: the average driving distance of n runs
+            [1]: standard deviation over those n runs.
+    """
+    drive_times = [drive() for _ in range(n)]
+    std_dev = np.std(drive_times)
+    avg_drive_dist = np.average(drive_times)
+    return (avg_drive_dist, std_dev)
 
 
-# calculates average driving dsitance over 10,000 drives
-def ten_k(): return sum(drive() for _ in range(10_000))/10_000
+trip = n_drives(10_000)
+print(f"""the average distance that was travled over 10,000 trips is :  {trip[0]}
+and the standard deviation is {trip[1]}\n""")
 
-# calculates the average distance driven over 100,000 drives
-def hundred_k(): return sum(drive() for _ in range(100_000))/100_000
+trip = n_drives(100_000)
+print(f"""the average distance that was travled over 10,000 trips is :  {trip[0]}
+and the standard deviation is {trip[1]}\n""")
+
 
 # calculates the average of 10 iterations of the average distance over 10,000 runs
-ten_iter = sum(ten_k() for _ in range(10))/10
+ten_iter_trips = [n_drives(10_000) for _ in range(10)]
+it = []
+ten_iter = (
+    # averaging the averages of each iteration
+    sum(avg_dist[0] for avg_dist in ten_iter_trips)/10, 
+    # averaging the standard deviation of each iteration
+    sum(avg_dist[1] for avg_dist in ten_iter_trips)/10
+)  
 
-
-print(f"the average distance that was travled over 10,000 trips is is:  {ten_k()}")
-print(f"the average distance that was travled over 10 iterations of 10,000 trips is is: {ten_iter}")
-print(f"the average distance that was travled over 100,000 trips is is:  {hundred_k()}")
-
+print(f"""the average of the average distance that was travled over 10, 10,000 trip iterations is :  {ten_iter[0]}
+and the average standard deviation is {ten_iter[1]}\n""")
